eca2c83d61a988342aa3b3d6e49dad6564c918a2
rohitsaxena <rohitmsaxena@gmail.com>
2025-02-11 04:45:18 -0700

    - Initial installation of React Flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import daisyLogo from "/daisyui.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { ReactFlow } from "@xyflow/react";
 
-function App() {
-  const [count, setCount] = useState(0);
+import "@xyflow/react/dist/style.css";
 
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+export default function App() {
   return (
-    <>
-      <div className="bg-gray-100 flex items-center justify-center">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://v5.daisyui.com/" target="_blank">
-          <img src={daisyLogo} className="logo daisy" alt="Daisyui logo" />
-        </a>
-      </div>
-      <h1 className="text-5xl font-bold">Vite + React + DaisyUI</h1>
-      <div className="card">
-        <button
-          className="btn btn-neutral"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite, React, and DaisyUI logos to learn more
-      </p>
-    </>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    </div>
   );
 }
-
-export default App;
